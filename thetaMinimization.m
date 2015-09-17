c952a08f50e49879e436ec2f846bfbddcfd4eae8
Alekhya Jonnalagedda <Alekhya@Alekhyas-MBP.wv.cc.cmu.edu>
2015-09-17 10:47:18 -0400

    graphs done

--- thetaMinimization.m
+++ thetaMinimization.m
@@ -7,18 +7,25 @@ r_0 = .05;
 min_error = Inf;
 min_theta = -1;
 
+x = ones(1,100);
+y = ones(1,100);
+i = 1;
 for theta = 0:.01:1 %minimization
     approx = MCEuler(k,beta,theta,r_0,M,N);
     derived = ZCBderived(k,beta,theta,r_0);
     
-    
     error = sum((approx - derived).^2);
+    x(1,i) = theta;  
+    y(1,i) = error;
+    i=i+1;
     if (error <= min_error)
         min_theta = theta;
         min_error = error;
     end
     
 end
+
+plot(x,y)
 
 end
 
